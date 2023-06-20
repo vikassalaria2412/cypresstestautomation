f647b93bcc7807ffbf676e4403ababc748d44a61
Vikas Salaria <vsalaria@dminc.com>
2023-06-20 21:34:09 +0530

    Commit message

---
+++ cypress/integration/test1.js
@@ -0,0 +1,53 @@
+describe('My First Test Suite',function()
+{
+    it('Addition',function(){
+        // write calculator app test cases that has two inputs Enter first numebr and second number and one drop down operator where action like Addition,subtraction,multcliplication and division can be selected and click on submit button and verify the result
+        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.wait(2000)
+        cy.get('[name="txt1"]').type("2")
+        cy.get('[name="txt2"]').type("3")
+        cy.get('[name="op"]').select("Addition")
+        //cy.get('[type="submit"]').click()
+        
+    })
+
+    it('Subtraction',function(){
+        // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
+        // clear the inputs
+        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.get('[name="txt1"]').type("4")
+        cy.get('[name="txt2"]').type("3")
+        cy.get('[name="op"]').select("Subtraction")
+        // cy.get('[name="txt2"]').type("3")
+        // cy.get('[name="op"]').select("Addition")
+        //cy.get('[type="submit"]').click()
+        
+    })
+
+    it('Multiplication',function(){
+        // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
+        // clear the inputs
+        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.get('[name="txt1"]').type("6")
+        cy.get('[name="txt2"]').type("4")
+        cy.get('[name="op"]').select("multiplication")
+        // cy.get('[name="txt2"]').type("3")
+        // cy.get('[name="op"]').select("Addition")
+        //cy.get('[type="submit"]').click()
+        
+    })
+    it('Division',function(){
+        // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
+        // clear the inputs
+        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.get('[name="txt1"]').type("6")
+        cy.get('[name="txt2"]').type("4")
+        cy.get('[name="op"]').select("division")
+        // cy.get('[name="txt2"]').type("3")
+        // cy.get('[name="op"]').select("Addition")
+        //cy.get('[type="submit"]').click()
+        
+    })
+
+    
+})
