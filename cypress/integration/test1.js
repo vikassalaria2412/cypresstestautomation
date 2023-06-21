9b688a0862a6ccb1a50bdfb0e7f469db4e83ffdf
Vikas Salaria <vsalaria@dminc.com>
2023-06-22 02:34:03 +0530

    changed tomcat server url

--- cypress/integration/test1.js
+++ cypress/integration/test1.js
@@ -2,7 +2,7 @@ describe('My First Test Suite',function()
 {
     it('Addition Operation',function(){
         // write calculator app test cases that has two inputs Enter first numebr and second number and one drop down operator where action like Addition,subtraction,multcliplication and division can be selected and click on submit button and verify the result
-        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.visit("http://52.201.237.207:8080/myapp/")
         cy.wait(2000)
         cy.get('[name="txt1"]').type("2")
         cy.get('[name="txt2"]').type("3")
@@ -14,7 +14,7 @@ describe('My First Test Suite',function()
     it('Subtraction Operation',function(){
         // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
         // clear the inputs
-        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.visit("http://52.201.237.207:8080/myapp/")
         cy.get('[name="txt1"]').type("4")
         cy.get('[name="txt2"]').type("3")
         cy.get('[name="op"]').select("Subtraction")
@@ -27,7 +27,7 @@ describe('My First Test Suite',function()
     it('Multiplication Operation',function(){
         // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
         // clear the inputs
-        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.visit("http://52.201.237.207:8080/myapp/")
         cy.get('[name="txt1"]').type("6")
         cy.get('[name="txt2"]').type("4")
         cy.get('[name="op"]').select("multiplication")
@@ -39,7 +39,7 @@ describe('My First Test Suite',function()
     it('Division Operation',function(){
         // write second test case of subtraction in continuation on same browser and clear previous inputs and enter new inputs and verify the result
         // clear the inputs
-        cy.visit("http://54.167.122.198:8080/myapp/")
+        cy.visit("http://52.201.237.207:8080/myapp/")
         cy.get('[name="txt1"]').type("6")
         cy.get('[name="txt2"]').type("4")
         cy.get('[name="op"]').select("division")
